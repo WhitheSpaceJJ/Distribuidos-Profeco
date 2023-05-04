58b19720c32494f6fa2263843243c7e368e10c02
WhiteSpaceJJ <jose.orozco229141@potros.itson.edu.mx>
2023-05-03 22:49:53 -0600

    orden de los proyectos deacuerdo al diagrama

---
+++ profeco-service/src/main/java/com/mycompany/profeco/service/ProfecoService.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.profeco.service;
+
+/**
+ *
+ * @author josej
+ */
+public class ProfecoService {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
